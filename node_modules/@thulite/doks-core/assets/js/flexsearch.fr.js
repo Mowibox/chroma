7f1541bb20eb10a3a1b9758067b82db974149c24
Mowibox <ousmane.thiongane@outlook.com>
2025-06-22 15:04:01 +0200

    fixing test

--- node_modules/@thulite/doks-core/assets/js/flexsearch.fr.js
+++ node_modules/@thulite/doks-core/assets/js/flexsearch.fr.js
@@ -123,7 +123,7 @@ import Index from 'flexsearch';
 
   function buildIndex() {
     document.querySelector('.search-loading').classList.remove('d-none');
-    fetch('{{ "search-index.json" | absLangURL }}')
+    fetch('search-index.json')
         .then(r => r.json())
         .then(data => data.forEach(i => index.add(i)));
   }
